8a8bd9f5869318b23440faacddad496d40c0c1ec
Pavol Loffay <ploffay@redhat.com>
2019-05-16 17:09:16 +0200

    Automate release
    Signed-off-by: Pavol Loffay <ploffay@redhat.com>

---
+++ travis/release.sh
@@ -0,0 +1,34 @@
+#!/bin/env bash
+
+set -o errexit -o nounset -o pipefail
+
+safe_checkout_master() {
+  # We need to be on a branch to be able to create commits,
+  # and we want that branch to be master, which has been checked before.
+  # But we also want to make sure that we build and release exactly the tagged version, so we verify that the remote
+  # branch is where our tag is.
+  checkoutBranch=master
+  git checkout -B "${checkoutBranch}"
+  git fetch origin "${checkoutBranch}":origin/"${checkoutBranch}"
+  commit_local_master="$(git show --pretty='format:%H' ${checkoutBranch})"
+  commit_remote_master="$(git show --pretty='format:%H' origin/${checkoutBranch})"
+  if [ "$commit_local_master" != "$commit_remote_master" ]; then
+    echo "${checkoutBranch} on remote 'origin' has commits since the version under release, aborting"
+    exit 1
+  fi
+}
+
+if [[ "$TRAVIS_TAG" =~ ^release-[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+?$ ]]; then
+    echo "We are on release- tag"
+    echo "bumping versions and creating vX.Y.Z tag"
+    echo "final artifact will be published in build for the tag"
+    safe_checkout_master
+    version=$(echo "${TRAVIS_TAG}" | sed 's/^release-//')
+    versionMajorMinor=$(echo "${version}" | sed 's/\.[[:digit:]]$//')
+    cp -r ./content/docs/next-release/ ./content/docs/${versionMajorMinor}
+    sed -i -e "s/latest *=.*$/latest = \"${versionMajorMinor}\"/" config.toml
+    sed -i -e "s/binariesLatest *=.*$/binariesLatest = \"${version}\"/" config.toml
+    sed -i -e "s/versions *= *\[/versions = \[\"${versionMajorMinor}\"\,/" config.toml
+    git add config.toml ./content/docs/${versionMajorMinor}
+    git commit -m "Release ${version}" -s
+fi
