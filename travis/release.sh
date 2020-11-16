e8a8fb088eeccdf767c4375b2c03df556fab8dd1
Joe Elliott <number101010@gmail.com>
2020-11-16 17:46:06 -0500

    Two changes to fix the release process (#456)
    * Two small changes to fix release
    
    Signed-off-by: Joe Elliott <number101010@gmail.com>
    
    * removed space:
    
    Signed-off-by: Joe Elliott <number101010@gmail.com>

--- travis/release.sh
+++ travis/release.sh
@@ -33,9 +33,7 @@ if [[ "$TRAVIS_TAG" =~ ^release-[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+?$ ]]; t
     cp -r ./data/cli/next-release/ ${cliDocsTempDir}/data/cli/${versionMajorMinor}
     chmod a+w -R ${cliDocsTempDir}
     python ./travis/gen-cli-data.py ${versionMajorMinor} ${cliDocsTempDir}
-    chmod 644 -R ${cliDocsTempDir}
     mv ${cliDocsTempDir}/data/cli/${versionMajorMinor} ./data/cli/
-
     sed -i -e "s/latest *=.*$/latest = \"${versionMajorMinor}\"/" config.toml
     sed -i -e "s/binariesLatest *=.*$/binariesLatest = \"${version}\"/" config.toml
     sed -i -e "s/versions *= *\[/versions = \[\"${versionMajorMinor}\"\,/" config.toml
