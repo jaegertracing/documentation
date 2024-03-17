d163420f82b2c27b10ebba40b7fc87b29e148177
Ashutosh Srivastava <ashutosh3002@gmail.com>
2024-03-18 01:44:37 +0530

    Delete old spellcheckers
    Signed-off-by: Ashutosh Srivastava <ashutosh3002@gmail.com>

--- scripts/spellcheck.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-find content/ -name '*.md' -exec bash -c 'misspelled_words=$(aspell --encoding=utf-8 list --mode=markdown --lang=en --ignore-case --personal=./my_dictionary.txt < "$0" | sort | uniq); if [ -n "$misspelled_words" ]; then echo -e "File: $0\nMisspelled words:\n$misspelled_words\n---------------------------------------------------"; fi' {} \;
